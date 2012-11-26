c956f11876a399956eb8065afe11b0b543326cfd
Andrew Leonard <andy@hurricane-ridge.com>
2012-11-26 15:48:48 -0800

    Further customization of /etc/ldap.conf.

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,13 +9,28 @@
 # [*base*]
 #   /etc/ldap.conf: The distinguished name of the LDAP search base.
 #
+# [*bind_timelimit*]
+#   /etc/ldap.conf: Bind/connect time limit.
+#
 # [*binddn*]
 #   /etc/ldap.conf: The distinguished name to bind to the server with.
+#
+# [*bindpw*]
+#   /etc/ldap.conf: Credentials used by binddn to bind with.
 #
 # [*hosts*]
 #   Array used to populate "host" in /etc/ldap.conf: Your LDAP server. Must be
 #   resolvable without using LDAP. (Note that this module only supports using
 #   "host" and not "uri".)
+#
+# [*rootbinddn*]
+#   /etc/ldap.conf: DN to bind to the server with if EUID is 0.
+#
+# [*scope*]
+#   /etc/ldap.conf: Search scope.
+#
+# [*timelimit*]
+#   /etc/ldap.conf: Search time limit.
 #
 # === Examples
 #
@@ -33,8 +48,13 @@
 #
 class ldap (
   $base = 'dc=example,dc=net',
+  $bind_timelimit = 30,
   $binddn = false,
-  $hosts = [ '127.0.0.1' ]
+  $bindpw = false,
+  $hosts = [ '127.0.0.1' ],
+  $rootbinddn = false,
+  $scope = 'sub',
+  $timelimit = 30
   ) {
 
   case $::operatingsystem {
