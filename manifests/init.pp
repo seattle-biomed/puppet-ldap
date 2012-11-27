b55e0f1beb43439fec06e8fce854988d7d6e050f
Andrew Leonard <andy@hurricane-ridge.com>
2012-11-26 16:46:55 -0800

    Get all standard configurables into /etc/ldap.conf.

--- manifests/init.pp
+++ manifests/init.pp
@@ -23,14 +23,53 @@
 #   resolvable without using LDAP. (Note that this module only supports using
 #   "host" and not "uri".)
 #
+# [*map_cn*]
+#   /etc/ldap.conf: cn attribute mapping.
+#
+# [*map_gecos*]
+#   /etc/ldap.conf: gecos attribute mapping.
+#
+# [*map_gidNumber*]
+#   /etc/ldap.conf: gidNumber attribute mapping.
+#
+# [*map_homeDirectory*]
+#   /etc/ldap.conf: homeDirectory attribute mapping.
+#
+# [*map_loginShell*]
+#   /etc/ldap.conf: loginShell attribute mapping.
+#
 # [*map_posixAccount*]
 #   /etc/ldap.conf: posixAccount objectclass mapping.
+#
+# [*map_posixGroup*]
+#   /etc/ldap.conf: posixGroup objectclass mapping.
+#
+# [*map_shadowAccount*]
+#   /etc/ldap.conf: shadowAccount objectclass mapping.
+#
+# [*map_uidNumber*]
+#   /etc/ldap.conf: uidNumber attribute mapping.
+#
+# [*map_uniqueMember*]
+#   /etc/ldap.conf: uniqueMember attribute mapping.
+#
+# [*map_userPassword*]
+#   /etc/ldap.conf: userPassword attribute mapping.
+#
+# [*pam_filter*]
+#   /etc/ldap.conf: Filter to AND searches with uid=%s
+#
+# [*pam_login_attribute*]
+#   /etc/ldap.conf: The user ID attribute (defaults to uid).
 #
 # [*pam_member_attribute*]
 #   /etc/ldap.conf: Group member attribute.
 #
 # [*pam_passwd*]
 #   /etc/ldap.conf: Define method for changing password on LDAP server.
+#
+# [*pam_password_prohibit_message*]
+#   /etc/ldap.conf: Redirect users to a URL or somesuch on password changes.
 #
 # [*rootbinddn*]
 #   /etc/ldap.conf: DN to bind to the server with if EUID is 0.
@@ -62,9 +101,22 @@ class ldap (
   $binddn = false,
   $bindpw = false,
   $hosts = [ '127.0.0.1' ],
+  $map_cn = false,
+  $map_gecos = false,
+  $map_gidNumber = false,
+  $map_homeDirectory = false,
+  $map_loginShell = false,
   $map_posixAccount = false,
+  $map_posixGroup = false,
+  $map_shadowAccount = false,
+  $map_uidNumber = false,
+  $map_uniqueMember = false,
+  $map_userPassword = false,
+  $pam_filter = false,
+  $pam_login_attribute = 'uid',
   $pam_member_attribute = 'uniquemember',
   $pam_passwd = 'md5',
+  $pam_password_prohibit_message = false,
   $rootbinddn = false,
   $scope = 'sub',
   $timelimit = 30
